de83e2fe5325e94df1cccce278a1cbd4685ec35b
Valentin Meltz <seth_valou@MacBook-Pro-de-Valentin-4.local>
2025-10-08 14:21:49 +0200

    update env.js

--- src/env.js
+++ src/env.js
@@ -1,4 +1,6 @@
-export default {
-    // Backend server URL
-    BACKEND_API_URL: process.env.BACKEND_URL,
+const env = {
+  // Backend server URL
+  BACKEND_API_URL: process.env.REACT_APP_BACKEND_URL,
 };
+
+export default env;
